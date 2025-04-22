582bc281bd0dcbb8bbda436a208a3eeb7d1836a2
Seeun Jung <seeun.jung01@gmail.com>
2025-04-22 10:40:42 +0900

    feat: 로그인 모달 구현

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -1,20 +1,58 @@
+import { useState } from "react";
 import "./Modal.css"
 
 export default function Modal({ onClose }) {
+  const [id, setId] = useState("");
+  const [password, setPassword] = useState("");
+  const [resultMessage, setResultMessage] = useState(null);
+
+  const sampleUser = {
+    id: "test123",
+    password: "test123pw"
+  };
+
+  const handleLogin = () => {
+    if (id === sampleUser.id && password === sampleUser.password){
+      setResultMessage(`${id}님 어서오세요!`);
+    } else{
+      setResultMessage("로그인 실패. 다시 시도해주세요.");
+    }
+  }
+
+  const handleResultClose = () => {
+    setResultMessage(null);
+    onClose();
+  };
+
   return (
     <div className="modalWrapper">
       <div className="modalContainer">
-        <div className="loginModalInner">
-            <p>
-              로그인 정보 입력
-            </p>
-            <input id="idInput" placeholder="id : exampleId" />
-            <input id="passwordInput" placeholder="pw : examplePassword" />
+      {resultMessage ? (
+          <div className="resultModalInner">
+            <p>{resultMessage}</p>
+            <button className="confirmBtn" onClick={handleResultClose}>확인</button>
+          </div>
+        ) : (
+          <div className="loginModalInner">
+            <p>로그인 정보 입력</p>
+            <input
+              value={id}
+              placeholder="id: test123"
+              onChange={(e) => setId(e.target.value)}
+              type="text"
+            />
+            <input
+              value={password}
+              placeholder="pw: test123pw"
+              onChange={(e) => setPassword(e.target.value)}
+              type="password"
+            />
             <div className="buttons">
-              <button>로그인</button>
+              <button onClick={handleLogin}>로그인</button>
               <button onClick={onClose}>취소</button>
             </div>
           </div>
+        )}
       </div>
     </div>
   );
